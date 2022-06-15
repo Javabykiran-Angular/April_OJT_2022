3292b92738724f1f89442ef85f70c135f269b1c4
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-06-15 16:03:58 +0530

    Day03

---
+++ Typescript/Day03/demo1.js
@@ -0,0 +1,30 @@
+// console.log("Hello World")
+// console.log('Sumit Raokhande');
+/*  */
+// var a,b;
+// var a1;
+// var a$b;
+// var a_b;
+//Data Types 
+// number(int,long,double,float,short) , string (""/''), booelan(true/false),
+// any (number,string,boolean)
+// void (netutral it does not point anything),
+//misec. null & Undefined it works data type as well as value 
+var a = 10; //Forward declaration & defination 
+// console.log('Value of a is '+a);
+var b1;
+// console.log(b1)
+var b2;
+// console.log(`value of B2 ${b2} `);
+b2 = 'Sumit Raokhande';
+// console.log('value of b2 is ',b2);
+var b3 = 45;
+var b4;
+b4 = 2.5;
+console.log("Value of b4 is " + b4);
+b4 = 'Sumit';
+console.log("Value of b4 is " + b4);
+b4 = true;
+console.log("Value of b4 is " + b4);
+var b5 = 12345634789102203000;
+console.log('Value of b5 is ' + b5);
---
+++ Typescript/Day03/demo1.ts
@@ -0,0 +1,43 @@
+
+// console.log("Hello World")
+// console.log('Sumit Raokhande');
+/*  */
+
+// var a,b;
+// var a1;
+// var a$b;
+// var a_b;
+
+//Data Types 
+// number(int,long,double,float,short) , string (""/''), booelan(true/false),
+// any (number,string,boolean)
+// void (netutral it does not point anything),
+//misec. null & Undefined it works data type as well as value 
+
+var a:number=10; //Forward declaration & defination 
+
+// console.log('Value of a is '+a);
+
+var b1!:number;
+// console.log(b1)
+
+var b2!:string;
+// console.log(`value of B2 ${b2} `);
+
+b2='Sumit Raokhande';
+// console.log('value of b2 is ',b2);
+
+var b3=45;
+var b4;
+b4=2.5;
+console.log("Value of b4 is "+b4);
+b4='Sumit';
+console.log("Value of b4 is "+b4);
+b4=true;
+console.log("Value of b4 is "+b4);
+var b5:number=123_45634_789_102_203_455;
+console.log('Value of b5 is '+b5);
+
+// var c1:string,c2:number;
+
+
