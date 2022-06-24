8558a184c48c68d5ec544e123603c68e45c272c7
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-06-24 15:57:23 +0530

    Day10

---
+++ Typescript/Day10/InterfaceMain.ts
@@ -0,0 +1,5 @@
+
+import {EmployeeDetails} from './employeedetails';
+
+let empobj=new EmployeeDetails('Sumit','Raokhande',100000,'DevOps');
+empobj.Display();
---
+++ Typescript/Day10/shape.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.Shape = void 0;
+var Shape = /** @class */ (function () {
+    function Shape() {
+    }
+    Shape.prototype.Myarea = function () {
+        console.log("U r in shape ");
+    };
+    return Shape;
+}());
+exports.Shape = Shape;
