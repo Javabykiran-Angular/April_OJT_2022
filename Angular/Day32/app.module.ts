5c36366634f43741ee3aae45bf3b1125f17922a9
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-08-13 16:19:04 +0530

    Day32

---
+++ Angular/Day32/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule,Title } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+
+import { ParticlesModule } from 'angular-particle'; 
+import { NgxCaptchaModule } from 'ngx-captcha';
+import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HeaderComponent } from './header/header.component';
+import { EmployeedetailsComponent } from './employeedetails/employeedetails.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material.module';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component'; 
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NotfoundComponent,
+    HeaderComponent,
+    EmployeedetailsComponent,
+    AddEmployeeComponent,
+    UpdateEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ParticlesModule,
+    NgxCaptchaModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ModalModule.forRoot()
+  ],
+  providers: [Title],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular/Day32/home/home.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { HttpService } from '../http.service';
+import { BsModalService,BsModalRef } from 'ngx-bootstrap/modal';
+import { Employee } from '../model/employee';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  Empdata:any[]=[];
+  empobj=<Employee>{};
+
+  isRdaioSelect:boolean=false;
+
+  modalRef: BsModalRef;  
+  config = {
+    animated: true,    
+    ignoreBackdropClick: true,
+    class: "alert alert-danger"
+  };
+
+  constructor(private service:HttpService,
+              private title:Title,
+              private modalservice:BsModalService ) { }
+
+  ngOnInit() {
+      this.getEmpData();
+      this.title.setTitle('Admin | Dashboard');
+  }
+
+  getEmpData(){
+      this.service.GetAllEmployeeData()
+      .subscribe((response)=>{
+        // console.log(response)
+        this.Empdata=(<any>response);
+               
+      })
+  }
+
+
+  onEdit(item){
+    // console.log(item)
+    this.isRdaioSelect=true;
+    this.empobj=item;
+  }
+
+  IsRadioCheck(){
+    if(this.isRdaioSelect)
+    return true;
+    else
+    return false
+  }
+
+  onUpdate(popup: TemplateRef<any>){
+    if(this.IsRadioCheck()){
+      //update a record
+      
+      this.modalRef = this.modalservice.show(popup, this.config);
+
+    }else{
+      alert("Please Select a record to Update !!!")
+    }
+
+  }
+
+
+}
